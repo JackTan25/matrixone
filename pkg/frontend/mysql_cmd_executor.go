153ede22cbb2cfcd53bb13fdfc20b30a0e4e8d54
JackTan25 <jacktby@gmail.com>
2023-01-30 09:08:27 +0800

    test

--- pkg/frontend/mysql_cmd_executor.go
+++ pkg/frontend/mysql_cmd_executor.go
@@ -4174,7 +4174,7 @@ func checkNodeCanCache(p *plan2.Plan) bool {
 	}
 	if q, ok := p.Plan.(*plan2.Plan_Query); ok {
 		for _, node := range q.Query.Nodes {
-			if node.NodeType == plan.Node_EXTERNAL_SCAN && node.TableDef.TableType == "query_result" {
+			if node.NotCacheable {
 				return false
 			}
 		}
--- pkg/vm/engine/disttae/types.go
+++ pkg/vm/engine/disttae/types.go
@@ -110,6 +110,8 @@ type Partition struct {
 	columnsIndexDefs []ColumnsIndexDef
 	// last updated timestamp
 	ts timestamp.Timestamp
+	// used for block read in PartitionReader
+	txn *Transaction
 }
 
 // Transaction represents a transaction
@@ -121,7 +123,9 @@ type Transaction struct {
 	// db       *DB
 	// blockId starts at 0 and keeps incrementing,
 	// this is used to name the file on s3 and then give it to tae to use
-	blockId uint64
+	// not-used now
+	// blockId uint64
+
 	// use for solving halloween problem
 	statementId uint64
 	// local timestamp for workspace operations
@@ -162,8 +166,6 @@ type Entry struct {
 	databaseName string
 	// blockName for s3 file
 	fileName string
-	// blockId for s3 file
-	blockId uint64
 	// update or delete tuples
 	bat     *batch.Batch
 	dnStore DNStore
